843b93756412bc4e6b2f13d7f7461c48abf4e047
MinseongKim <alstjdk7@kaist.ac.kr>
2019-01-08 01:02:47 +0900

    Added all

--- pictures/views.py
+++ pictures/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 # Create your views here.
@@ -9,3 +10,7 @@ def post_list(request):
     published_date__lte=timezone.now()\
     ).order_by('published_date')
   return render(request, "pictures/post_list.html", {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'pictures/post_detail.html', {'post': post})
